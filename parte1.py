68486827f3cd0e49f329b613996c90e2781b31fe
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2023-02-16 19:15:53 -0600

    parte 1

--- parte1.py
+++ parte1.py
@@ -4,33 +4,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-""" #leer el archivo
-with open('BCW.data') as f:
-    reader = csv.reader(f)
-    data = [row for row in reader]
+""" # Se lee el .data
+df = pd.read_csv('BCW.data', header=None)
 
-#limpiar el archivo y convertir los numeros a integers
-cleaned_data = []
-for row in data:
-    cleaned_row = []
-    for value in row:
-        try:
-            cleaned_value = int(value)
-        except ValueError:
-            # Si hay valores que no tienen un numero se sustituye por un 0
-            cleaned_value = 0
-        cleaned_row.append(cleaned_value)
-    cleaned_data.append(cleaned_row)
+#se agregan los titulos de las columnas
+df.columns = ['id', 'clump_thickness', 'cell_size_uniformity', 'cell_shape_uniformity',
+              'marginal_adhesion', 'single_epithelial_cell_size', 'bare_nuclei',
+              'bland_chromatin', 'normal_nucleoli', 'mitoses', 'class']
 
- #poner la información limpia en un csv
-with open('cleaned_file1.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(cleaned_data) """
+# si hay un caracter que no es numero, pasarlo a cero
+df = df.apply(pd.to_numeric, errors='coerce').fillna(0)
+
+#Cambiar tipo de las columnas
+df['id'] = df['id'].astype('object')
+df['class'] = df['class'].astype('int')
+df[df.columns[1:-1]] = df[df.columns[1:-1]].astype('int')
+
+# Nuevo csv
+df.to_csv('cleaned_file.csv', index=False) """
+
 
 
 #Exploracion de datos
 
-""" data = pd.read_csv('cleaned_file.csv')
+data = pd.read_csv('cleaned_file.csv')
 
 print("Data shape")
 print(data.shape, "\n")
@@ -41,17 +38,32 @@ print("Data basic statistics")
 print(data.describe(), "\n")
 
 print(data.iloc[:10, :4])
- """
 
 
-data = pd.read_csv('cleaned_file.csv')
+
+
+#Tipos de datos y aislamiento
+""" data = pd.read_csv('cleaned_file.csv')
 
 print("Data types")
 print(data.dtypes, "\n")
 
+# Aislar las variables numéricas y categóricas
+numeric_vars = df.select_dtypes(include=['int', 'float'])
+categorical_vars = df.select_dtypes(include=['object'])
+
+# Calcular la matriz de correlación entre las variables numéricas
+correlation_matrix = numeric_vars.corr()
+
+# Visualizar la matriz de correlación
+plt.matshow(correlation_matrix)
+plt.xticks(range(len(numeric_vars.columns)), numeric_vars.columns, rotation=90)
+plt.yticks(range(len(numeric_vars.columns)), numeric_vars.columns)
+plt.colorbar()
+plt.show() """
 
 
-
+#Histogramas
 """plt.hist(data['Cell size'],edgecolor="white")
 plt.title('Histogram of Cell size')
 plt.xlabel('Cell size')
@@ -63,4 +75,27 @@ plt.show() """
 plt.title('Histogram of thickness')
 plt.xlabel('Thickness')
 plt.ylabel('Frequency')
+plt.show() """
+
+
+
+#Diagrama de caja y bigote
+""" x = data['Adhesion']
+y = data['Cell size']
+
+sns.boxplot(x=x, y=y)
+plt.show()
+ """
+
+#Heatmap
+""" corr = data.corr()
+
+sns.heatmap(corr, annot=True)
+plt.show() """
+
+
+#Pie chart con porcentaje de tumores tipo 2 y 4
+""" counts = data['Class'].value_counts()
+
+plt.pie(counts, labels=counts.index, autopct='%1.1f%%')
 plt.show() """
